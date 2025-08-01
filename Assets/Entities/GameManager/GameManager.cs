e6b26f7ecaaed6c797fcc25987b9f273b9553cf8
Lucas RiedlShah <lucasp.riedlshah@gmail.com>
2025-08-01 14:18:32 +1200

    Set up basic GameStates

--- Assets/Entities/GameManager/GameManager.cs
+++ Assets/Entities/GameManager/GameManager.cs
@@ -1,6 +1,71 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class GameManager
+public enum GameState
 {
-    
+    MainMenu,
+    Gameplay,
+    Pause,
+    GameOver
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameState currentState;
+    private InputAction pauseAction;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        pauseAction = InputSystem.actions.FindAction("Pause");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (currentState)
+        {
+            case GameState.MainMenu:
+                MainMenu();
+                break;
+            case GameState.Gameplay:
+                Gameplay();
+                break;
+            case GameState.Pause:
+                Pause();
+                break;
+            case GameState.GameOver:
+                GameOver();
+                break;
+        }
+    }
+
+    void MainMenu()
+    {
+        Debug.Log("MainMenu");
+        currentState = GameState.Gameplay;
+    }
+
+    void Gameplay()
+    {
+        Debug.Log("Gameplay");
+        if (pauseAction.WasPressedThisFrame())
+        {
+            currentState = GameState.Pause;
+        }
+    }
+
+    void Pause()
+    {
+        Debug.Log("Pause");
+        if (pauseAction.WasPressedThisFrame())
+        {
+            currentState = GameState.Gameplay;
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("GameOver");
+    }
 }
