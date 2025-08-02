76241a84023add01384badb044d922dfd454112d
deDug <rswe356@aucklanduni.ac.nz>
2025-08-03 02:23:54 +1200

    Amazing death saving bananza

--- Assets/Managers/PlayerManager/CharacterThread.cs
+++ Assets/Managers/PlayerManager/CharacterThread.cs
@@ -1,5 +1,7 @@
 // Stores history of one character for the current loop
+using System;
 using System.Collections.Generic;
+using System.Threading;
 
 
 namespace PlayerManager {
@@ -12,12 +14,47 @@ namespace PlayerManager {
 
         public Character threadCharacter;
 
+        // If a loop ended and this was active and not dead
+        public enum ThreadState { 
+                                  Unplayed, // Has not yet been touched by human hands this loop
+                                  Active, // This character is active
+                                  Inactive, // Has been active and died TODO rename to something slightly descriptive of what it is
+                                  Complete, // Completed loop
+
+                                  PermenantlyDead // Dead as dead can be, died and cannot be brought back 
+                                };
+
+        private int limitingDeathTick; // This is the tick which, if survived, will allow a replay
+        private int lastLoopDeathTick; // The last loop death tick, if this is greater than the limiting tick we can replay
+
+        public ThreadState state = ThreadState.Unplayed;
+
         public CharacterThread(Character threadChar)
         {
             history = new();
-            threadTicks = 0;
             threadCharacter = threadChar;
+            Reset();
         }        
+
+        // This records info about death timings so that, when a character is saved
+        // that information can be tracked and acted on
+        public void SetDeathInfo(int currentTick)
+        {
+            lastLoopDeathTick = currentTick;
+
+            // If we died while active, update the limiting tick
+            if (state == ThreadState.Active)
+            {
+                // If we are active and die, this is the furthest we have
+                // got 'for realsies' (meaning while under player control)
+                // This is the tick to beat!
+                if (limitingDeathTick > currentTick)
+                {
+                    throw new InvalidOperationException("Shouldn't be able to die while active earlier than you did before");
+                } 
+                limitingDeathTick = currentTick;
+            }
+        }
 
         public void PushHistory(PlayerInputRecord record)
         {
@@ -25,17 +62,52 @@ namespace PlayerManager {
             threadTicks++;
         }
 
+        public bool IsUnderPlayerControl(int currentTick)
+        {
+            if (state != ThreadState.Active)
+            {
+                throw new InvalidOperationException("Should only be called on active threads!");
+            }
+
+            if (currentTick >= limitingDeathTick)
+            {
+                return true;            
+            }
+            return false;
+        }
+
         public void Reset()
         {
             history.Clear();
             threadTicks = 0;
+            limitingDeathTick = 0;
+            lastLoopDeathTick = 0;
+
+            // We latch into permenant death state
+            if (state != ThreadState.PermenantlyDead)
+            {
+                state = ThreadState.Unplayed;
+            }
         }
 
         // Returns true if this thread can be selected in a character selection phase
-        // First implementation, if thread ticks are zero?
         public bool CanActivate()
         {
-            return threadTicks == 0;
+            // If thread hasn't been played yet, we can always select them
+            if (state == ThreadState.Unplayed)
+            {
+                return true;
+            }
+
+            // Else, if we have died we can activate if our last death was later than the limiting death tick
+            // TODO this is a bit odd since you could save them one looop, choose not to play them, then they die early 
+            //      and now you have 'wasted' that saving
+            if (state == ThreadState.Inactive && limitingDeathTick < lastLoopDeathTick)
+            {
+                return true;
+            } 
+
+            return false;
         }
     }
 }
--- Assets/Managers/PlayerManager/LoopStartState.cs
+++ Assets/Managers/PlayerManager/LoopStartState.cs
@@ -9,17 +9,27 @@ namespace PlayerManager {
         // For now, just transform
         public List<Vector3> characterPositions;
         public List<Quaternion> characterRotations;
+        public List<Character.CharacterState> characterStates; 
+
+        // public void()
+        // {
+            // characterPositions = new();
+            // characterRotations = new();
+            // characterStates = new();
+        // }
 
         public void SaveState(List<CharacterThread> threads)
         { 
             characterPositions.Clear();
             characterRotations.Clear();
+            characterStates.Clear();
 
             foreach (var thread in  threads)
             {
                 var character = thread.threadCharacter;
                 characterPositions.Add(character.transform.position);
                 characterRotations.Add(character.transform.rotation);
+                characterStates.Add(character.state);
             }
         }
 
@@ -30,6 +40,16 @@ namespace PlayerManager {
                 var character = threads[i].threadCharacter;
                 character.transform.position = characterPositions[i];
                 character.transform.rotation = characterRotations[i];
+
+                Debug.Log($"Character state is: {character.state}");
+                Debug.Log($"Saved state is: {characterStates[i]}");
+
+
+                if (characterStates[i] == Character.CharacterState.Alive && character.state == Character.CharacterState.Dead)
+                {
+                    Debug.Log("Undied...");
+                    character.UnDie();
+                }
             }
         }
     }
