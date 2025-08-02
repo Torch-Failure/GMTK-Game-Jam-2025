bb0a94b5a6e2697c9b925cf13b5baf626e6fa09e
samuel-reedy <sam.reedy@bduncanltd.com>
2025-08-02 15:32:41 +1200

    Removed everything from enemy manager

--- Assets/Managers/EnemyManager.cs
+++ Assets/Managers/EnemyManager.cs
@@ -5,18 +5,5 @@ using UnityEngine;
 
 public class EnemyManager : MonoBehaviour
 {
-    private List<GameObject> enemies = new List<GameObject>();
-    private List<GameObject> players = new List<GameObject>();
-
-    void GetEnemies()
-    {
-        enemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
-        Debug.Log($"Found {enemies.Count} enemies");
-    }
-
-    void GetPlayers()
-    {
-        players = new List<GameObject>(GameObject.FindGameObjectsWithTag("Player"));
-        Debug.Log($"Found {players.Count} players");
-    }
+    
 }
